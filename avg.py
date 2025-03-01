b5098b639154bdd38236f8f84511192a44bf16cd
venkateshwari <donuri.venkateshwari@gmail.com>
2025-03-01 12:49:14 +0530

    added comment

--- avg.py
+++ avg.py
@@ -2,6 +2,8 @@
 # b=int(input("enter number 2:"));
 # c=(a+b)/2
 # print("the avg of given numbers is:",c);
+# here we have added 2 numbers with user input
+
 
 max_num= lambda x,y:x if x>y else y
 print (max_num(10,20))
