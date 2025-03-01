8c87cf15a21ab85f7703cdd95a8a28d5c6272b9b
venkateshwari <donuri.venkateshwari@gmail.com>
2025-03-01 12:27:25 +0530

    added avg file

---
+++ avg.py
@@ -0,0 +1,7 @@
+# a=int(input("enter number 1:"));
+# b=int(input("enter number 2:"));
+# c=(a+b)/2
+# print("the avg of given numbers is:",c);
+
+max_num= lambda x,y:x if x>y else y
+print (max_num(10,20))
