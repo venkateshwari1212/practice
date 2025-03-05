64d4fd4a20167f72288f08b5dae1937717951fc8
venkateshwari <donuri.venkateshwari@gmail.com>
2025-03-05 22:54:40 +0530

    Added a loop to print numbers from 1 to N

--- projects_70.py
+++ projects_70.py
@@ -6,10 +6,10 @@ print("Hello,World!")
 # print("Hello,",name,'!')
 
 # 3.swap two numbers
-a=10 #assigning 10 to a  
-b=5 # assigning 5 to b
-a,b=b,a # swapping the values 
-print(a,b)
+# a=10 #assigning 10 to a  
+# b=5 # assigning 5 to b
+# a,b=b,a # swapping the values 
+# print(a,b)
 
 # 4.check if a number is even or odd 
 # let us take input x with user to check given number is even or odd
@@ -90,14 +90,96 @@ it must also be divisible by 400 to be a leap year.'''
 
 # 3. Check if a character is a vowel or consonant
 
-character=input("enter a char:")
-vowels= "aeiou"
-if character in vowels:
-    print(f"{character} is vowel")
-elif character.isalpha():
-    print(f"{character} is consonant")
+# character=input("enter a char:")
+# vowels= "aeiou"
+# if character in vowels:
+#     print(f"{character} is vowel")
+# elif character.isalpha():
+#     print(f"{character} is consonant")
+# else: 
+#     print(f"{character} is invalid input please enter letters")
+
+# 4. Write a Python program to assign the grades to each subject based on the grade scale: 
+#  Student subject marks:
+#  maths = 85
+#  science = 78
+#  english = 92
+#  Grade scale:
+#  A: 90-100
+#  B: 80-89
+#  C: 70-79
+#  D: 60-69
+#  E: 50-59
+#  F: Below 50 
+
+maths=85
+science=78
+english=92
+if maths<=100 and maths>=90:
+    maths_grade= "A"
+elif maths<=89 and maths>=80:
+    maths_grade= "B"
+elif maths<=79 and maths>=70:
+    maths_grade= "C"
+elif maths<=69 and maths>=60:
+    maths_grade= "D"
+elif maths<=59 and maths>=50:
+    maths_grade= "E"
+elif maths<=49:
+    maths_grade= "F"
+else:
+    print(f"{maths} invalid marks")
+
+if science<=100 and science>=90:
+    science_grade= "A"
+elif science<=89 and science>=80:
+    science_grade= "B"
+elif science<=79 and science>=70:
+    science_grade= "C"
+elif science<=69 and science>=60:
+    science_grade= "D"
+elif science<=59 and science>=50:
+    science_grade= "E"
+elif science<=49:
+    science_grade= "F"
+else:
+    print(f"{science} invalid marks")
+
+if english<=100 and english>=90:
+    english_grade= "A"
+elif english<=89 and english>=80:
+    english_grade= "B"
+elif english<=79 and english>=70:
+    english_grade= "C"
+elif english<=69 and english>=60:
+    english_grade= "D"
+elif english<=59 and english>=50:
+    english_grade= "E"
+elif english<=49:
+    english_grade= "F"
+else:
+    print(f"{english} invalid marks")
+
+print(f"maths marks-{maths}: maths grade-{maths_grade}")
+print(f"science marks-{maths}: maths grade-{science_grade}")
+print(f"english marks-{english}: maths grade-{english_grade}")
+
+'''5. Write a program to check if a number is divisible by both 5 and 11
+Python Programming'''
+
+number=20
+if number%5==0 and number%11==0:
+    print(f"{number} is divisable by both 5 and 11")
 else: 
-    print(f"{character} is invalid input please enter letters")
+    print(f"{number} is not divisible by 5 and 11")
+
+# Loops
+#1. Print numbers from 1 to the given number N
+N=int(input("enter a number to get numbers: "))
+i=0
+while i<N:
+    i=i+1
+    print(i)
 
 
 
